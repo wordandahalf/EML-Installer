f59c719f80756ad68f9723f36382d9d8892920ed
Ryan J <mail2ryanjones@gmail.com>
2018-12-16 23:51:11 -0600

    Added method of finding the Steam directory using the registry

--- src/io/github/wordandahalf/equilinox/modloader/installer/Main.java
+++ src/io/github/wordandahalf/equilinox/modloader/installer/Main.java
@@ -2,10 +2,19 @@ package io.github.wordandahalf.equilinox.modloader.installer;
 
 import java.io.IOException;
 
+import io.github.wordandahalf.equilinox.modloader.installer.utils.WindowsUtils;
 import io.github.wordandahalf.equilinox.modloader.installer.web.WebFetcher;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		System.out.println("Running on " + System.getProperty("os.name"));
+		
+		if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+			String steamDirectory = WindowsUtils.readRegistry("HKEY_CURRENT_USER\\Software\\Valve\\Steam", "SteamPath");
+		
+			System.out.println("Steam is installed at " + steamDirectory);
+		}
+		
 		if(WebFetcher.canConnect())
 			System.out.println("The update server is online!");
 		
---
+++ src/io/github/wordandahalf/equilinox/modloader/installer/utils/WindowsUtils.java
@@ -0,0 +1,61 @@
+package io.github.wordandahalf.equilinox.modloader.installer.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+public class WindowsUtils {
+	/**
+	 * 
+	 * @param location path in the registry
+	 * @param key      registry key
+	 * @return registry value or null if not found
+	 */
+	public static final String readRegistry(String location, String key) {
+		try {
+			// Run reg query, then read output with StreamReader (internal class)
+			Process process = Runtime.getRuntime().exec("reg query " + '"' + location + "\" /v " + key);
+
+			StreamReader reader = new StreamReader(process.getInputStream());
+			reader.start();
+			process.waitFor();
+			reader.join();
+			String output = reader.getResult();
+			
+			// Output has the following format:
+			// \n<location>\n\t<key>\t<key type>\t<value>
+			if (!output.contains("    ") && !output.contains("\t")) {
+				return null;
+			}
+
+			// Parse out the value
+			String[] parsed = output.split("    ");
+			return parsed[parsed.length - 1].replace(System.getProperty("line.separator"), "");
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	static class StreamReader extends Thread {
+		private InputStream is;
+		private StringWriter sw = new StringWriter();
+
+		public StreamReader(InputStream is) {
+			this.is = is;
+		}
+
+		public void run() {
+			try {
+				int c;
+				while ((c = is.read()) != -1)
+					sw.write(c);
+			} catch (IOException e) {
+			}
+		}
+
+		public String getResult() {
+			return sw.toString();
+		}
+	}
+}
